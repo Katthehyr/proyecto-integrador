d48b0520d4086ab181e9b133e1e16b574573b35e
“Monica <monkatthyr@gmail.com>
2024-08-07 12:56:24 -0500

    realizando las peticiones

--- main.js
+++ main.js
@@ -1,35 +1,33 @@
 import './css/style_index.css';
 
-window.addEventListener('DOMContentLoaded', function(){})
+import Handlebars from "handlebars";
 
-const start = async () => {
+const template = Handlebars.compile("Name: {{name}}");
+console.log(template({ name: "Nils" }));
+
+
+window.addEventListener('DOMContentLoaded', async function(){
     console.log('Hola mundo')
-}
 
-try {
-
-    const respuesta = await fetch('templates/card.hbs')
+    try {
+        const respuesta = await fetch('templates/card.hbs')
     
-    const plantilla = await respuesta.text()
+        const plantilla = await respuesta.text();
+    
+        console.log(plantilla)
 
-    console.log(plantilla)
-
-    const template = Handlebars.compile(plantilla)
-
-    const respuestaBack = await fetch('http://localhost:8080/productos')
+        const respuestaBack = await fetch('http://localhost:8080/Productos/')
 
     if ( !respuestaBack.ok) {
         throw new Error('Algo paso con los productos', respuestaBack.status) }
 
-    const dataProductos = await respuestaBack.json()
+    const dataProductos = await respuestaBack.json();
     console.log(dataProductos)
 
-    const data = { productos: dataProductos}
+    const data = { Productos: dataProductos}
     console.log(data)
-
-    const html = template(data)
-
-    console.log(html)
-
+    
+    } catch (error) {
+        console.log(error)
     }
-        
+})
