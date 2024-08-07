62ff7f0c5d5d813b0bbc7e64f407c938dd7087f4
“Monica <monkatthyr@gmail.com>
2024-08-07 14:50:14 -0500

    final segunda etapa

--- main.js
+++ main.js
@@ -16,7 +16,8 @@ window.addEventListener('DOMContentLoaded', async function(){
     
         console.log(plantilla)
 
-        const respuestaBack = await fetch('http://localhost:8080/Productos/')
+
+        const respuestaBack = await fetch('https://66b3b9c17fba54a5b7edfcbc.mockapi.io/productos/')
 
     if ( !respuestaBack.ok) {
         throw new Error('Algo paso con los productos', respuestaBack.status) }
